573a516f724e1d906e707bd68b8b1887319ca32a
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2026-06-03 09:41:30 +0200

    feat(user-authorities): add user authorities fetch and state management
    Create a recoil atom to store user's authorities.
    Add a custom hook to fetch user authorities from the DHIS2 me endpoint.
    Update AppWrapper to handle loading and error states for the authorities data.

---
+++ src/hooks/users/useGetUserAthorities.ts
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import { useDataQuery } from '@dhis2/app-runtime'
+import { useSetRecoilState } from 'recoil'
+import { UserAuthoritiesSchema } from '../../schema/userAuthoritiesSchema'
+
+const meQuery = {
+    me: { resource: 'me' },
+}
+
+export const useGetUserAuthorities = () => {
+    const { loading, error, data } = useDataQuery(meQuery)
+    const setAuthorities = useSetRecoilState(UserAuthoritiesSchema)
+
+    useEffect(() => {
+        if (data?.me) {
+            console.log((data.me as any).authorities)
+            setAuthorities((data.me as any).authorities || [])
+        }
+    }, [data, setAuthorities])
+
+
+    return { loading, error }
+}
